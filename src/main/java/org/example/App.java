efcfb86328c19c2dbbd2fbbc6dd666b3f34ab07d
darkstar100x <darkstar100x@gmail.com>
2021-11-27 15:20:03 -0500

    Third Commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,39 +1,21 @@
 package org.example;
 
-import org.jgrapht.Graph;
 import org.jgrapht.alg.flow.EdmondsKarpMFImpl;
 import org.jgrapht.graph.DefaultDirectedWeightedGraph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.util.SupplierUtil;
-import java.util.Map;
 
 public class App {
   public static void main(String[] args) {
     DefaultDirectedWeightedGraph<Vertex, DefaultEdge> G = new DefaultDirectedWeightedGraph<>(SupplierUtil.createSupplier(Vertex.class), SupplierUtil.DEFAULT_EDGE_SUPPLIER);
     Vertex[] vertices = new Vertex[10];
-    generateGraph2(G, vertices);
-    System.out.println("Result: " + MaximumFlow.edmundKarp(G, vertices[0], vertices[6]));
+    generateGraph1(G, vertices);
+    System.out.println("My Result: " + MaximumFlow.edmundKarp(G, vertices[0], vertices[6]));
     EdmondsKarpMFImpl<Vertex, DefaultEdge> flow = new EdmondsKarpMFImpl<>(G);
-    System.out.println(flow.calculateMaximumFlow(vertices[0], vertices[6]));
+    System.out.println("Should Be: " + (int) flow.calculateMaximumFlow(vertices[0], vertices[6]));
   }
 
-  public static void generateGraph1(Graph<Vertex, DefaultEdge> G, Map<DefaultEdge, Double> W, Vertex[] vertices) {
-    for (int i = 0; i < 8; i++) {
-      vertices[i] = G.addVertex();
-    }
-    W.put(G.addEdge(vertices[0], vertices[3]), 7.0);
-    W.put(G.addEdge(vertices[1], vertices[3]), 3.0);
-    W.put(G.addEdge(vertices[1], vertices[2]), 7.0);
-    W.put(G.addEdge(vertices[1], vertices[6]), 1.0);
-    W.put(G.addEdge(vertices[2], vertices[0]), 2.0);
-    W.put(G.addEdge(vertices[4], vertices[7]), 3.0);
-    W.put(G.addEdge(vertices[5], vertices[1]), 1.0);
-    W.put(G.addEdge(vertices[6], vertices[4]), 7.0);
-    W.put(G.addEdge(vertices[6], vertices[7]), 7.0);
-    W.put(G.addEdge(vertices[7], vertices[5]), 1.0);
-  }
-
-  public static void generateGraph2(DefaultDirectedWeightedGraph<Vertex, DefaultEdge> G, Vertex[] vertices) {
+  public static void generateGraph1(DefaultDirectedWeightedGraph<Vertex, DefaultEdge> G, Vertex[] vertices) {
     for (int i = 0; i < 7; i++) {
       vertices[i] = G.addVertex();
     }
--- src/main/java/org/example/MaximumFlow.java
+++ src/main/java/org/example/MaximumFlow.java
@@ -13,50 +13,45 @@ public class MaximumFlow {
     }
     int maxFlow = 0;
     while (true) {
-      DefaultEdge[] path = breadthFirstSearch(G, s, t, flow);
-      if (path == null) {
+      breadthFirstSearch(G, s, t, flow);
+      if (t.getPi() == null) {
         break;
       }
       int cf = Integer.MAX_VALUE;
-      for (DefaultEdge e: path) {
+      for (Vertex v = t; v.getPi() != null; v = v.getPi()) {
+        DefaultEdge e = G.getEdge(v.getPi(), v);
         int cv = (int) G.getEdgeWeight(e) - flow.get(e);
         cf = Integer.min(cf, cv);
       }
-      for (DefaultEdge e: path) {
-        if (G.containsEdge(e)) {
-          flow.put(e, flow.get(e) + cf);
-        }
-        else if (G.containsEdge(G.getEdgeTarget(e), G.getEdgeSource(e))) {
-          flow.put(G.getEdge(G.getEdgeTarget(e), G.getEdgeSource(e)), flow.get(G.getEdge(G.getEdgeTarget(e), G.getEdgeSource(e))) - cf);
-        }
+
+      for (Vertex v = t; v.getPi() != null; v = v.getPi()) {
+        DefaultEdge e = G.getEdge(v.getPi(), v);
+        flow.put(e, flow.get(e) + cf);
       }
       maxFlow += cf;
     }
     return maxFlow;
   }
 
-  public static DefaultEdge[] breadthFirstSearch(DefaultDirectedWeightedGraph<Vertex, DefaultEdge> G, Vertex s, Vertex t, Map<DefaultEdge, Integer> flow) {
-    ArrayList<DefaultEdge> edgesInPath = new ArrayList<>();
+  public static void breadthFirstSearch(DefaultDirectedWeightedGraph<Vertex, DefaultEdge> G, Vertex s, Vertex t, Map<DefaultEdge, Integer> flow) {
+    for (Vertex v: G.vertexSet()) {
+      v.setPi(null);
+    }
+    Set<Vertex> visited = new HashSet<>();
     Queue<Vertex> q = new ArrayDeque<>();
-    boolean check = false;
     q.add(s);
     while (!q.isEmpty()) {
       Vertex cur = q.remove();
-      for (DefaultEdge e: G.edgesOf(cur)) {
-        if ((!edgesInPath.contains(e)) && (!G.getEdgeTarget(e).equals(s)) && (G.getEdgeWeight(e) > flow.get(e))) {
-          edgesInPath.add(e);
+      visited.add(cur);
+      if (cur.equals(t)) {
+        return;
+      }
+      for (DefaultEdge e: G.outgoingEdgesOf(cur)) {
+        if (!(G.getEdgeTarget(e).equals(s)) && !(visited.contains(G.getEdgeTarget(e))) && (G.getEdgeWeight(e) > flow.get(e))) {
           q.add(G.getEdgeTarget(e));
+          G.getEdgeTarget(e).setPi(cur);
         }
       }
-      if (cur.equals(t)) {
-        check = true;
-      }
-    }
-    if (check) {
-      return edgesInPath.toArray(new DefaultEdge[0]);
-    }
-    else {
-      return null;
     }
   }
 }
